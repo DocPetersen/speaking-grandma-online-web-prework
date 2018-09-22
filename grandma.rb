7a065bacf84a0b2c57f96c42dac1b68ecb960d02
REESE PETERSEN <reese.r.petersen@gmail.com>
2018-09-22 04:17:38 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,13 +1,13 @@
 require "pry"
 
 def speak_to_grandma(phrase)
-  if 
+   if phrase == "I LOVE YOU GRANDMA!"
+      "I LOVE YOU TOO PUMPKIN!"
     
   elsif phrase == phrase.upcase
     put "NO, NOT SINCE 1938!"
     
   else 
     "HUH?! SPEAK UP, SONNY!"
-    if phrase == "I LOVE YOU GRANDMA!"
-      "I LOVE YOU TOO PUMPKIN!"
   end
+end
